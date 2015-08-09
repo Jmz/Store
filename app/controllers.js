ae64ead74ba7173597652d41e5192e2964579161
James Owers <jamesowers@gmail.com>
2015-08-09 12:17:38 +0100

    Added category tree menu.Updated theme.

--- app/controllers.js
+++ app/controllers.js
@@ -2,6 +2,12 @@
 
 angular.module('store.controllers', [])
 
+	.controller('NavController', ['$scope', 'categoryService', function($scope, categoryService) {
+		categoryService.getTree().then(function(categories) {
+			$scope.categories = categories;
+		});
+	}])
+
 	.controller('HomePageController', ['$scope', 'products', function($scope, products) {
-    $scope.products = products;
+    	$scope.products = products;
 	}]);
--- app/services.js
+++ app/services.js
@@ -2,11 +2,29 @@
 
 angular.module('store.services', [])
 
-	.factory('auth', function($q) {
+	.factory('auth', ['$q', function($q) {
 		var deferred = $q.defer();
 		var moltin = new Moltin({publicId: 'blGyfNGUQZkMqktoOGOPXypYnV0Hu8f6cuDjRAfx'});
 		moltin.Authenticate(function() {
 			deferred.resolve(moltin);
 		});
 		return deferred.promise;
-	});
+	}])
+
+	.factory('categoryService', ['$q', 'auth', function($q, auth) {
+		var categoryServiceObject = {};
+
+		categoryServiceObject.getTree = function() {
+	        var deferred = $q.defer();
+	        auth.then(function(moltin) {
+	          moltin.Category.Tree(null, function(categories) {
+	            deferred.resolve(categories);
+	          });
+	        });
+	        return deferred.promise;
+		}
+
+		return categoryServiceObject;
+	}])
+
+;
